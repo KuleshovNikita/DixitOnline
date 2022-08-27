c90733489538c776dd1050bdc65977cddae37483
KuleshovNikita <79578426+KuleshovNikita@users.noreply.github.com>
2022-08-27 22:28:56 +0300

    created DB contex, added a Player DTO

---
+++ DixitOnline.Api/Controllers/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using DixitOnline.Models.PlayerData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DixitOnline.Api.Controllers
+{
+    [Route("players")]
+    public class PlayerController : Controller
+    {
+        [HttpGet]
+        public PlayerModel GetPlayer()
+        {
+            return new PlayerModel
+            {
+                Name = "Mykyta",
+                PlayerId = 1
+            };
+        }
+    }
+}
---
+++ DixitOnline.DataAccess/Migrations/20220827191936_CreatedDixitDataBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DixitOnline.DataAccess.Migrations
+{
+    public partial class CreatedDixitDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    PlayerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.PlayerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
