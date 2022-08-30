91e5efafbbd8c7b435df88b8e1f089e317046264
KuleshovNikita <79578426+KuleshovNikita@users.noreply.github.com>
2022-08-30 09:24:58 +0300

    added initial logic

--- UI/dixit-ui/src/App.js
+++ UI/dixit-ui/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './css-common/InputField.css';
+import './css-common/Button.css';
+import PopUpMessage from './components/PopUpMessage/PopUpMessage';
 
 function App() {
+  const [playerName, setPlayerName] = React.useState('');
+  const [isError, setIsError] = React.useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Dixit Online</h1>
+        <input className="InputField" placeholder='Enter your name' type={"text"} onChange={handleChange}/>
+        <button className='Button' onClick={registerNewPlayer}>Start New Game</button> 
+        <button className='Button' >Connect To The Room</button> 
+        { isError && <PopUpMessage message="Name cannot be empty!" type='error' showMessage={() => showPopup(false)}/> }
     </div>
   );
+
+  function showPopup(toggle) {
+    setIsError(toggle);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function registerNewPlayer(event) {
+    if(playerName === '') {
+      setIsError(true);
+    }
+
+    var player = {Name: playerName, PlayerId: 1};
+
+    fetch('https://localhost:44303/players/newPlayer',
+    { method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': "application/json;charset=utf-8"
+      },
+      body: JSON.stringify(player),
+    }).catch(err => console.log(err));
+
+    event.preventDefault();
+  }
 }
 
 export default App;
